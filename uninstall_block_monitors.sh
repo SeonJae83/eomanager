6bb625198ce8bb44dec09aa4ea742ae47d553003
SeonJae83 <137477287+SeonJae83@users.noreply.github.com>
2025-06-12 12:04:12 +0900

    Update uninstall_block_monitors.sh

--- uninstall_block_monitors.sh
+++ uninstall_block_monitors.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "스크립트 및 서비스 제거를 시작합니다..."
-
 # 1. 서비스 중지 및 비활성화
 sudo systemctl stop squid-ip-monitor.service
 sudo systemctl disable squid-ip-monitor.service
@@ -9,19 +7,20 @@ sudo systemctl stop dante-ip-monitor.service
 sudo systemctl disable dante-ip-monitor.service
 
 # 2. 서비스 파일 삭제
-sudo rm -f /etc/systemd/system/squid-ip-monitor.service
-sudo rm -f /etc/systemd/system/dante-ip-monitor.service
+sudo rm -rf /etc/systemd/system/squid-ip-monitor.service
+sudo rm -rf /etc/systemd/system/dante-ip-monitor.service
 
 # 3. systemd 데몬 리로드
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
 
 # 4. 개별 스크립트 파일 삭제
-rm -f /home/script/dante-ip-block-monitor.sh
-rm -f /home/script/squid-ip-block-monitor.sh
-rm -f /home/script/lock-list.sh
-rm -f /home/script/unlock-ip.sh
+rm -rf /home/script/dante-ip-block-monitor.sh
+rm -rf /home/script/squid-ip-block-monitor.sh
+rm -rf /home/script/install_block_monitors_v3.2.sh
+rm -rf /home/script/lock-list.sh
+rm -rf /home/script/unlock-ip.sh
+rm -rf /home/script/uninstall_block_monitors.sh
 
 # 5. 로그디렉토리 삭제 
-rm -f /home/script/logs
-echo "삭제 완료"
+rm -rf /home/script/logs
